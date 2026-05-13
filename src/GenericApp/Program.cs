759c43be22006f2c67af9e21c796ea6dc803425d
Meantralif <itulupnikov@gmail.com>
2026-05-13 19:31:27 +0300

    Learn and practice Generics

---
+++ src/GenericApp/Program.cs
@@ -0,0 +1,38 @@
+﻿Storage<int> intStorage = new();
+intStorage.SetValue(42);
+Console.WriteLine($"Int: {intStorage.GetValue()}");
+
+Storage<string> strStorage = new();
+strStorage.SetValue("Hello");
+Console.WriteLine($"String: {strStorage.GetValue()}");
+
+class Storage<T> {
+    private T value;
+
+    public void SetValue(T val) {
+        value = val;
+    }
+
+    public T GetValue() {
+        return value;
+    }
+}
+
+// class IntStorage {
+//     private int value;
+//     public void SetValue(int val) {
+//         value = val;
+//     }
+//     public int GetValue() {
+//         return value;
+//     }
+// }
+// class StringStorage {
+//     private string? value;
+//     public void SetValue(string val) {
+//         value = val;
+//     }
+//     public string GetValue() {
+//         return value;
+//     }
+// }
